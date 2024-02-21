33733662dd6dd34d1889173116905a918f9e81db
Scott Sanya Emonanekkul <scottse@stud.ntnu.no>
2024-02-21 20:23:16 +0100

    started on  exercise5 and created  necessary files to solve the tasks

---
+++ exercise5/script.js
@@ -0,0 +1,14 @@
+async function runCode() {
+    const code = document.getElementById('code').value;
+
+    const response = await fetch('/runcode', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ code }),
+    });
+
+    const result = await response.text();
+    document.getElementById('result').innerText = result;
+}
---
+++ exercise5/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { exec } = require('child_process');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/runcode', (req, res) => {
+    const { code } = req.body;
+
+    // Write code to a temporary Java file
+    const fileName = 'temp.java';
+    require('fs').writeFileSync(fileName, code);
+
+    // Run the Java code using Docker
+    exec(`docker run --rm -v "$(pwd)":/usr/src/app -w /usr/src/app openjdk:latest javac ${fileName} && java ${fileName.replace('.java', '')}`, (error, stdout, stderr) => {
+        if (error) {
+            res.send(stderr);
+        } else {
+            res.send(stdout);
+        }
+    });
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
